91dd39d4d18da2ca6d5195a174bd67cec9a13a12
andreujuanjane <andreujuanjane@192.168.1.33>
2013-01-16 18:01:12 +0100

    He esborrat el ServiceLocator, que no es fa servir perque ja tenim el client del Servei Web

--- Eclipse/src/servicelocator/BankService.java
+++ Eclipse/src/servicelocator/BankService.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 /** Dummy class per que de moment no peti tot **/
 public class BankService {
-
+    
 	public Boolean autoritza(String dniClient, String numTarg, float preu, Date dataCad) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
